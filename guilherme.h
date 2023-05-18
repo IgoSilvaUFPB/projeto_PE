d3046678c60246b459fde25a69e603f5bef57665
Igo Silva <igo.silva@academico.ufpb.br>
2023-05-18 11:00:17 -0300

    instrucoes guilherme

--- guilherme.h
+++ guilherme.h
@@ -2,3 +2,6 @@
 #define _guilherme_H_ 
 
 //Sua área de trabalho
+//Função que lê os arquivos com as coordenadas dos pontos e dos centroides e plota num gráfico
+//Algumas opções: OpenGL, SDV, GTK+, Gnuplot, MathGL, Plotly, cairo, PLplot, ChartDirector, Dislin, Allegro, FreeGLUT, e Allegro.
+//Algumas criam o arquivo e salvam na máquina. Mas dê prerência àquelas que já abrem o gráfico em tempo de execução, para que possamos ver a mágica acontecer.
