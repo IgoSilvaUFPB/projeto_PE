d09c49e038c3a3f93d24ce56336e6a00730b2788
Francelino jr <115588340+Francelinojr@users.noreply.github.com>
2023-05-18 11:59:01 -0300

    Update francelino.h

--- francelino.h
+++ francelino.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 #define MAX_ITERATIONS 100
 #define NUM_POINTS 200
@@ -25,74 +27,144 @@ double distance(Point point, Centroid centroid) {
     return sqrt(pow(point.x - centroid.x, 2) + pow(point.y - centroid.y, 2));
 }
 
-int main() {
-    Point points[NUM_POINTS];
-    Centroid centroids[NUM_CLUSTERS];
-    
-    // Inicialização: Selecionar centroides aleatórios
-    centroids[0].x = 1.0;
-    centroids[0].y = 1.0;
-    centroids[0].count = 0;
-    centroids[1].x = -1.0;
-    centroids[1].y = -1.0;
-    centroids[1].count = 0;
-    centroids[2].x = 0.0;
-    centroids[2].y = 0.0;
-    centroids[2].count = 0;
-    
-    // Leitura dos pontos de dados
+void readData(Point points[], Centroid centroids[]) {
     FILE *file = fopen("df_test.csv", "r");
     if (file == NULL) {
         printf("Erro ao abrir o arquivo.\n");
-        return 1;
+        exit(1);
     }
-    
+
     char line[100];
     fgets(line, sizeof(line), file);  // Ignorar o cabeçalho do arquivo
-    
+
     for (int i = 0; i < NUM_POINTS; i++) {
         fgets(line, sizeof(line), file);
         sscanf(line, "%*d,%lf,%lf", &points[i].x, &points[i].y);
         points[i].cluster = 0;
     }
-    
+
     fclose(file);
-    
-    // Execução do algoritmo K-means
-    int iteration;
-    for (iteration = 0; iteration < MAX_ITERATIONS; iteration++) {
-        // Associação: Atribuir pontos aos centroides mais próximos
-        for (int i = 0; i < NUM_POINTS; i++) {
-            double minDistance = INFINITY;
-            int closestCentroid;
-            
-            for (int j = 0; j < NUM_CLUSTERS; j++) {
-                double d = distance(points[i], centroids[j]);
-                if (d < minDistance) {
-                    minDistance = d;
-                    closestCentroid = j;
-                }
-            }
-            
-            points[i].cluster = closestCentroid;
-            centroids[closestCentroid].count++;
-            centroids[closestCentroid].x += points[i].x;
-            centroids[closestCentroid].y += points[i].y;
-        }
-        
-        // Atualização: Recalcular as posições dos centroides
+}
+
+void assignPointsToClosestCentroids(Point points[], Centroid centroids[]) {
+    for (int i = 0; i < NUM_POINTS; i++) {
+        double minDistance = INFINITY;
+        int closestCentroid;
+
         for (int j = 0; j < NUM_CLUSTERS; j++) {
-            centroids[j].x /= centroids[j].count;
-            centroids[j].y /= centroids[j].count;
+            double d = distance(points[i], centroids[j]);
+            if (d < minDistance) {
+                minDistance = d;
+                closestCentroid = j;
+            }
+        }
+
+        points[i].cluster = closestCentroid;
+        centroids[closestCentroid].count++;
+        centroids[closestCentroid].x += points[i].x;
+        centroids[closestCentroid].y += points[i].y;
+    }
+}
+
+bool updateCentroids(Point points[], Centroid centroids[]) {
+    bool positionChanged = false;
+
+    for (int j = 0; j < NUM_CLUSTERS; j++) {
+        if (centroids[j].count > 0) {
+            double newX = centroids[j].x / centroids[j].count;
+            double newY = centroids[j].y / centroids[j].count;
+
+            if (newX != centroids[j].x || newY != centroids[j].y) {
+                positionChanged = true;
+            }
+
+            centroids[j].x = newX;
+            centroids[j].y = newY;
             centroids[j].count = 0;
         }
     }
-    
-    // Imprimir os resultados
-    printf("Resultado do K-means:\n");
-    for (int i = 0; i < NUM_POINTS; i++) {
-        printf("Ponto (%lf, %lf) pertence ao cluster %d\n", points[i].x, points[i].y, points[i].cluster);
+
+    return positionChanged;
+}
+
+void performKMeans(Point points[], Centroid centroids[]) {
+    int iteration;
+    bool positionChanged = true;
+
+    for (iteration = 0; iteration < MAX_ITERATIONS && positionChanged; iteration++) {
+        assignPointsToClosestCentroids(points, centroids);
+        positionChanged = updateCentroids(points, centroids);
+
+        // Mostrar o estado atual dos centroides e pontos atribuídos
+        printf("Iteração %d:\n", iteration + 1);
+        for (int j = 0; j < NUM_CLUSTERS; j++) {
+            printf("Centroide %d: (%lf, %lf)\n", j + 1, centroids[j].x, centroids[j].y);
+        }
+        printf("\n");
+        for (int i = 0; i < NUM_POINTS; i++) {
+            printf("Ponto (%lf, %lf) pertence ao cluster %d\n", points[i].x, points[i].y, points[i].cluster);
+        }
+        printf("\n");
+
+        usleep(1000000);  // Pausa de 1 segundo entre iterações
     }
-    
+
+    printf("Número de iterações: %d\n", iteration);
+}
+
+void writeCentroidsToFile(Centroid centroids[]) {
+    FILE *file = fopen("centroide_resultado.csv", "w");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+
+    fprintf(file, "Centroide,X,Y\n");
+    for (int j = 0; j < NUM_CLUSTERS; j++) {
+        fprintf(file, "%d,%lf,%lf\n", j + 1, centroids[j].x, centroids[j].y);
+    }
+
+    fclose(file);
+}
+
+int main() {
+    Point points[NUM_POINTS];
+    Centroid centroids[NUM_CLUSTERS];
+
+    // Criação de uma cópia dos arquivos com as coordenadas iniciais dos pontos e centroides
+    FILE *pointsFile = fopen("df_test.csv", "r");
+    FILE *centroidsFile = fopen("centroide_test.csv", "r");
+    FILE *pointsCopyFile = fopen("df_test_copy.csv", "w");
+    FILE *centroidsCopyFile = fopen("centroide_test_copy.csv", "w");
+
+    if (pointsFile == NULL || centroidsFile == NULL || pointsCopyFile == NULL || centroidsCopyFile == NULL) {
+        printf("Erro ao abrir os arquivos.\n");
+        exit(1);
+    }
+
+    char line[100];
+    while (fgets(line, sizeof(line), pointsFile)) {
+        fputs(line, pointsCopyFile);
+    }
+
+    while (fgets(line, sizeof(line), centroidsFile)) {
+        fputs(line, centroidsCopyFile);
+    }
+
+    fclose(pointsFile);
+    fclose(centroidsFile);
+    fclose(pointsCopyFile);
+    fclose(centroidsCopyFile);
+
+    // Leitura dos dados
+    readData(points, centroids);
+
+    // Execução do algoritmo K-means
+    performKMeans(points, centroids);
+
+    // Gravar os centroides em um arquivo
+    writeCentroidsToFile(centroids);
+
     return 0;
 }
+
