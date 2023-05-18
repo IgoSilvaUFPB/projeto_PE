176c3f206c35d2020fe98acb846e5c64f3159662
Igo Silva <igo.silva@academico.ufpb.br>
2023-05-18 11:57:49 -0300

    intrucoes Guilhereme 2

--- guilherme.h
+++ guilherme.h
@@ -2,6 +2,12 @@
 #define _guilherme_H_ 
 
 //Sua área de trabalho
+
 //Função que lê os arquivos com as coordenadas dos pontos e dos centroides e plota num gráfico
 //Algumas opções: OpenGL, SDV, GTK+, Gnuplot, MathGL, Plotly, cairo, PLplot, ChartDirector, Dislin, Allegro, FreeGLUT, e Allegro.
 //Algumas criam o arquivo e salvam na máquina. Mas dê prerência àquelas que já abrem o gráfico em tempo de execução, para que possamos ver a mágica acontecer.
+/*
+Além dos pontos e dos centroides, também deverá ser representada a atribuíção de cada ponto ao centroide.
+Inicialmente ninguém é de ninguém, mas a cada iteração os pontos serão atribuídos a um centroide.
+Isso é importante mostrar para ver como as coisas transcorreram.
+*/
