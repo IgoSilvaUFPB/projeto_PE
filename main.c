4a2a33a895624c3337ac5b7c9c61ecf50b1ba421
Igo Silva <igo.silva@academico.ufpb.br>
2023-05-18 09:09:42 -0300

    adicionando o main

---
+++ main.c
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------*/
+//Univerdide Federal da Paraíba - UFPB
+//Curso: Ciência de Dados e Inteligência Artificial
+//Disciplina: Programação Estruturada
+//Projeto: Implementação do algoritmo K-Means na linguagem C
+//Equipe:
+// - DAVI NASIASENE AMORI (20220056987)
+// - FRANCELINO TEOTONIO JUNIOR (20190035175)
+// - GUILHERME BARBOZA DE SOUSA (20220007418)
+// - IGÓ FERREIRA MELO SILVA (20220155214)
+/*---------------------------------------------------------*/
+#include <stdio.h>
+
+int main(void) {
+  printf("K-Means\n");
+  return 0;
+}
