fed8b539b5546683e9c5d54cef68fea20039855c
Igo Silva <igo.silva@academico.ufpb.br>
2023-05-18 11:18:24 -0300

    instrucoes davi

--- davi.h
+++ davi.h
@@ -2,3 +2,15 @@
 #define _davi_H_ 
 
 //Sua área de trabalho
+
+/*
+Função para gerar as coordenadas iniciais dos centroides aleatoriamente.
+E salvar em um arquivo CSV no mesmo formato do centroide_test.csv.
+*/
+
+/*
+Função para gerar as coordenadas iniciais dos pontos, 200 pontos, aleatoriamente,
+mas no mesmo esquema do gerador_csv_cluster.ipynb que está no repositório.
+Deixando os pontos concentrados em 3 regiões do gráfico.
+E salvar em um arquivo CSV no mesmo formato do df_test.csv.
+*/
